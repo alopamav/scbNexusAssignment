e0a3b48ce2c3331a0fd0cd5c1d39d3917ee1309a
Alopama Vishnupriya <alopamav@maveric-systems.com>
2020-07-02 17:22:17 +0530

    Updates

--- precache-manifest.0413be552a18b0cb0cd3eabb738bea45.js
+++ precache-manifest.0413be552a18b0cb0cd3eabb738bea45.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "daf8450c4cd8c6b0c56bd4f38d652d2f",
+    "revision": "fd7bff2353566e75e7386311d14b9d3d",
     "url": "/scbNexusAssignment/index.html"
   },
   {
-    "revision": "7d1d3d3a22275817f1f3",
+    "revision": "2fce5afd4e98e5eb0df5",
     "url": "/scbNexusAssignment/static/css/main.5ecd60fb.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/scbNexusAssignment/static/js/2.a4a92119.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7d1d3d3a22275817f1f3",
-    "url": "/scbNexusAssignment/static/js/main.adddaca0.chunk.js"
+    "revision": "2fce5afd4e98e5eb0df5",
+    "url": "/scbNexusAssignment/static/js/main.fecd72cb.chunk.js"
   },
   {
     "revision": "af4afbea7a722fc965b7",
